ad4a7d092bb226a9d68fb3cda1d4379f0b1fb94c
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-28 23:32:36 +0530

    [Sachin] Adding Interface

---
+++ Interfaces/InerFace.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    // Create the interface class
+    interface IEmployee 
+    {
+        //Declare the function 
+        void show();
+    }
+       // Create another interface
+    interface IEmployee2
+    {
+        //Declare 2 function
+        void show2();
+    }
+        // We are implemented in derived class two interface
+        // and implemented multiple inheritance
+    class PartTimeEmployee:IEmployee,IEmployee2
+    {
+        public void show()
+        {
+            Console.WriteLine("This is a method of IEmployee Interface");
+        }
+        public void show2()
+        {
+            Console.WriteLine("This is the Second interface method");
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the interface");
+            PartTimeEmployee pte = new PartTimeEmployee();
+            pte.show();
+            pte.show2();
+        }
+    }
+}
