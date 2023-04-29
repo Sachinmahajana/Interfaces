7f629331861d14c8d1772958c630ca19ba7db146
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-29 10:11:22 +0530

    [Sachin] Adding programs

---
+++ Interfaces/InterFaceInheritance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    //interface inheriting each other
+    //interface 1 declare here
+    interface I1
+    {
+         void print();
+    }
+    //interface 2 declare here
+    interface I2
+    {
+         void print2();
+    }
+    //interface3 declare here and inherit with each other
+    interface I3:I1,I2
+    {
+         void print3();
+    }
+   public  class Programs: I3
+   {
+        public void print()
+        {
+            Console.WriteLine("These is the I1 interface method");
+        }  
+        public void print2()
+        {
+            Console.WriteLine("These is the I2 interface method");
+        }
+        public void print3()
+        {
+            Console.WriteLine("These is the I3 interface method");
+        }
+    }
+
+}
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -5,9 +5,31 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the interface");
-            PartTimeEmployee pte = new PartTimeEmployee();
-            pte.show();
-            pte.show2();
+            Console.WriteLine("Select the Program given below");
+            Console.WriteLine("\n1:Interface\n2:Interfaceinheritance");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    PartTimeEmployee pte = new PartTimeEmployee(); 
+                    pte.show();
+                    pte.show2();
+                    break;
+                case 2:
+                    //Programs program = new Programs();
+                    //program.print();
+                    //program.print2();
+                    //program.print3();
+                    // reference variable of parent class I3
+                    I3 myinterface = new Programs();
+                    myinterface.print();
+                    myinterface.print2();
+                    myinterface.print3();
+                    break;
+                default:
+                    Console.WriteLine("Please select program given below");
+                    break;
+            }
         }
     }
 }
